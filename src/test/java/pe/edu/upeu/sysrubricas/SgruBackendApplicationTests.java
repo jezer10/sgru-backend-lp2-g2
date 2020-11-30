1458c3da1ed091decea1773cfbfa3b3549e45a89
SaulFernand0 <saulolivas@upeu.edu.pe>
2020-11-29 21:02:20 -0500

    primer avance

--- src/test/java/pe/edu/upeu/sysrubricas/SgruBackendApplicationTests.java
+++ src/test/java/pe/edu/upeu/sysrubricas/SgruBackendApplicationTests.java
@@ -1,14 +1,20 @@
 package pe.edu.upeu.sysrubricas;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import pe.edu.upeu.sysrubricas.dao.Sys_numericoDao;
 
 @SpringBootTest
 class SgruBackendApplicationTests {
+	
+	@Autowired
+	private Sys_numericoDao sys_numericoDao;
 
 	@Test
 	void contextLoads() {
-		System.out.println("MODO YAPU");
+		System.out.println(sys_numericoDao.readAll());
 	}
 
 }
